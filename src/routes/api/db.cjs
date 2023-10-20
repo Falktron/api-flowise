2d485ad2a011d793b66653df3a7b797be7923e4f
Falktron <96954004+Falktron@users.noreply.github.com>
2023-10-20 13:01:26 +0200

    Update db.cjs

--- src/routes/api/db.cjs
+++ src/routes/api/db.cjs
@@ -6,11 +6,11 @@ const app = express();
 
 
 const pool = new pg.Pool({
-  user: process.env.DATABASE_USERNAME  || 'flowise',
-  password: process.env.DATABASE_PASSWORD ||'Change_this_password',
-  host: process.env.DATABASE_HOST|| 'cxo2.mium.chat',
-  database: process.env.DATABASE_NAME||'flowise',
-  port: process.env.DATABASE_PORT||'5432',
+  user: process.env.DATABASE_USERNAME ,
+  password: process.env.DATABASE_PASSWORD ,
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  port: process.env.DATABASE_PORT,
 });
 
 
