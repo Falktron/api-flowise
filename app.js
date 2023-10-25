6e6f7e75fc8c64714c16e7f25388e9b9850579da
Falktron <96954004+Falktron@users.noreply.github.com>
2023-10-25 15:29:53 +0200

    api update

--- app.js
+++ app.js
@@ -5,44 +5,111 @@ const port = process.env.PORT || 3001;
 const pg = require('pg');
 
 
+
 const pool = new pg.Pool({
-  user: process.env.DATABASE_USERNAME  ,
-  password: process.env.DATABASE_PASSWORD ,
+  user: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
   host: process.env.DATABASE_HOST,
-  database: process.env.DATABASE_NAME ,
+  database: process.env.DATABASE_NAME,
   port: process.env.DATABASE_PORT,
+});
+
+
+
+
+// Enable CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.get('/test', async (req, res) => {
+  try {
+    console.log('Request received for /test endpoint');
+    const result = await pool.query('SELECT results FROM pipeline');
+    console.log('Query executed successfully');
+    console.log('Result:', result.rows);
+    res.json(result.rows);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/audience', async (req, res) => {
+  try {
+    console.log('Request received for /audience endpoint');
+    const result = await pool.query(
+      'SELECT results FROM pipeline WHERE project_id = $1 AND pipeline_name = $2',
+      [req.query.projectId, req.query.pipelineName]
+    );
+    console.log('Query executed successfully');
+    console.log('Result:', result.rows);
+    res.json(result.rows);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+const fetchPrediction = async (url, data) => {
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Authorization: "Bearer gDqzGFaOSHeOKe4Sc6Js1iZg1RuQERr8po8TgDKMGHE=",
+        "Content-Type": "application/json"
+      },
+      method: "POST",
+      body: JSON.stringify(data)
+    });
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error("Error fetching data from the API");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    throw new Error("Internal server error");
+  }
+};
+
+app.post('/target-users', async (req, res) => {
+  console.log("gola");
+  try {
+    
+    const result = await fetchPrediction("http://75.119.157.23:3001/api/v1/prediction/df3a83b7-60da-43d6-ac2f-8939a5e86b72", req.body);
+    console.log(req.body);
+    res.json(result);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post('/business-model', async (req, res) => {
+  try {
+    const result = await fetchPrediction("http://75.119.157.23:3001/api/v1/prediction/58f9feec-0b72-4f4a-9d29-bf5671c976a2", req.body);
+    res.json(result);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post('/marketing-research', async (req, res) => {
+  try {
+    const result = await fetchPrediction("http://75.119.157.23:3001/api/v1/prediction/afa6f22a-60f4-4a13-a644-d522ed749562", req.body);
+    res.json(result);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: error.message });
+  }
 });
 
 app.get("/", (req, res) => res.type('html').send(html));
 
-
-app.get('/test', async (req, res) => {
-  try {
-    console.log('Request received for /result endpoint');
-    const result = await pool.query(
-      'SELECT results FROM pipeline',
-    );
-    res.json(result.rows);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-app.get('/result', async (req, res) => {
-  try {
-    console.log('Request received for /result endpoint');
-    const { project_id, pipeline_name } = req.query; // Assuming the parameters are passed as query parameters in the URL
-    const result = await pool.query(
-      'SELECT results FROM pipeline WHERE project_id = $1 AND pipeline_name = $2',
-      [project_id, pipeline_name] // Pass the parameters as an array in the same order as the placeholders ($1, $2, etc.)
-    );
-    res.json(result.rows);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
--- server/apiService.js
+++ server/apiService.js
@@ -1,3 +1,4 @@
+require('dotenv').config(); // Add this line at the top of your file
 import express from 'express';
 import pg from 'pg';
 import fetch from "node-fetch";
@@ -11,6 +12,9 @@ const pool = new pg.Pool({
   database: process.env.DATABASE_NAME,
   port: process.env.DATABASE_PORT,
 });
+
+
+
 
 // Enable CORS
 app.use((req, res, next) => {
@@ -28,7 +32,7 @@ app.get('/test', async (req, res) => {
     console.log('Result:', result.rows);
     res.json(result.rows);
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error('Error:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
@@ -44,15 +48,14 @@ app.get('/audience', async (req, res) => {
     console.log('Result:', result.rows);
     res.json(result.rows);
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error('Error:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-app.post("/target-users", async (req, res) => {
+const fetchPrediction = async (url, data) => {
   try {
-    const data = req.body;
-    const response = await fetch("http://75.119.157.23:3001/api/v1/prediction/df3a83b7-60da-43d6-ac2f-8939a5e86b72", {
+    const response = await fetch(url, {
       headers: {
         Authorization: "Bearer gDqzGFaOSHeOKe4Sc6Js1iZg1RuQERr8po8TgDKMGHE=",
         "Content-Type": "application/json"
@@ -60,70 +63,47 @@ app.post("/target-users", async (req, res) => {
       method: "POST",
       body: JSON.stringify(data)
     });
-    const result = await response.json();
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error("Error fetching data from the API");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    throw new Error("Internal server error");
+  }
+};
+
+app.post('/target-users', async (req, res) => {
+  console.log("gola");
+  try {
+    
+    const result = await fetchPrediction("http://75.119.157.23:3001/api/v1/prediction/df3a83b7-60da-43d6-ac2f-8939a5e86b72", req.body);
+    console.log(req.body);
     res.json(result);
   } catch (error) {
     console.error("Error:", error);
-    res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ error: error.message });
   }
 });
 
-app.post("/target-users", async (req, res) => {
+app.post('/business-model', async (req, res) => {
   try {
-    const response = await fetch("http://75.119.157.23:3001/api/v1/prediction/df3a83b7-60da-43d6-ac2f-8939a5e86b72", {
-      headers: {
-        Authorization: "Bearer gDqzGFaOSHeOKe4Sc6Js1iZg1RuQERr8po8TgDKMGHE=",
-        "Content-Type": "application/json"
-      },
-      method: "POST",
-      body: JSON.stringify(req.body)
-    });
-
-    if (response.ok) res.json(await response.json());
-    else throw new Error("Error fetching data from the API");
+    const result = await fetchPrediction("http://75.119.157.23:3001/api/v1/prediction/58f9feec-0b72-4f4a-9d29-bf5671c976a2", req.body);
+    res.json(result);
   } catch (error) {
     console.error("Error:", error);
-    res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ error: error.message });
   }
 });
 
-
-
-app.post("/business-model", async (req, res) => {
+app.post('/marketing-research', async (req, res) => {
   try {
-    const data = req.body;
-    const response = await fetch("http://75.119.157.23:3001/api/v1/prediction/58f9feec-0b72-4f4a-9d29-bf5671c976a2", {
-      headers: {
-        Authorization: "Bearer gDqzGFaOSHeOKe4Sc6Js1iZg1RuQERr8po8TgDKMGHE=",
-        "Content-Type": "application/json"
-      },
-      method: "POST",
-      body: JSON.stringify(data)
-    });
-    if (response.ok) res.json(await response.json());
-    else throw new Error("Error fetching data from the API");
+    const result = await fetchPrediction("http://75.119.157.23:3001/api/v1/prediction/afa6f22a-60f4-4a13-a644-d522ed749562", req.body);
+    res.json(result);
   } catch (error) {
     console.error("Error:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-app.post("/marketing-research", async (req, res) => {
-  try {
-    const data = req.body;
-    const response = await fetch("http://75.119.157.23:3001/api/v1/prediction/afa6f22a-60f4-4a13-a644-d522ed749562", {
-      headers: {
-        Authorization: "Bearer gDqzGFaOSHeOKe4Sc6Js1iZg1RuQERr8po8TgDKMGHE=",
-        "Content-Type": "application/json"
-      },
-      method: "POST",
-      body: JSON.stringify(data)
-    });
-    if (response.ok) res.json(await response.json());
-    else throw new Error("Error fetching data from the API");
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ error: error.message });
   }
 });
 
